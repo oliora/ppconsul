12458c63ae1adc571c2a10b3c7e7e48be79dad63
Andrey Upadyshev <oliora@gmail.com>
2014-09-26 03:55:15 +0200

    HTTP client wrapper implemented

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -1,23 +1,98 @@
 #include "http_client.h"
+#include <boost/network/protocol/http/client.hpp>
 
 
 namespace ppconsul { namespace http {
 
-    class Client::Impl {};
+    namespace http = boost::network::http;
 
-    Client::Client(const char *host)
-    {}
-
-    std::string Client::get(Status& status, const char *path, const Options& options)
+    const char *BadStatus::what() const PPCONSUL_NOEXCEPT
     {
-        return {};
+        char prefix[] = "HTTP ";
+
+        if (m_what.empty())
+        {
+            // 3 is for status code + space - NULL of prefix
+            m_what.reserve((sizeof(prefix) / sizeof(prefix[0])) + m_status.message().size() + 3);
+            m_what = prefix;
+            m_what += std::to_string(m_status.code());
+            m_what += ' ';
+            m_what += m_status.message();
+        }
+
+        return m_what.c_str();
     }
 
-    void Client::put(Status& status, const char *path, const std::string& body, const Options& options)
+    std::string Parameters::query() const
+    {
+        // TODO: encode parameters?
+
+        if (m_values.empty())
+            return {};
+
+        std::string r;
+
+        // 2 is for '=' and '&'. -1 is because first param has no '&'
+        auto len = std::accumulate(m_values.begin(), m_values.end(), -1, [](int s, const Parameter& p) {
+            return s + p.m_name.size() + p.m_value.size() + 2;
+        });
+
+        r.reserve(len - 1);
+
+        auto it = m_values.begin();
+
+        r += it->m_name;
+        r += '=';
+        r += it->m_value;
+
+        for(;it != m_values.end(); ++it)
+        {
+            r += '&';
+            r += it->m_name;
+            r += '=';
+            r += it->m_value;
+        }
+
+        return r;
+    }
+
+    class Client::Impl
+    {
+    public:
+        http::client m_client;
+    };
+
+    Client::Client(const char *host)
+    : m_impl(new Impl)
+    , m_host(host)
     {}
 
-    void Client::del(Status& status, const char *path, const Options& options)
+    Client::~Client()
     {}
+
+    std::string Client::get(Status& status, const char *path, const Parameters& parameters)
+    {
+        http::client::request request(createUri(path, parameters));
+        auto response = m_impl->m_client.get(request);
+        status = Status(response.status(), response.status_message());
+        return response.body();
+    }
+
+    void Client::put(Status& status, const char *path, const std::string& body, const Parameters& parameters)
+    {
+        http::client::request request(createUri(path, parameters));
+        // TODO: add content type header?
+        request.body(body);
+        auto response = m_impl->m_client.delete_(request);
+        status = Status(response.status(), response.status_message());
+    }
+
+    void Client::del(Status& status, const char *path, const Parameters& parameters)
+    {
+        http::client::request request(createUri(path, parameters));
+        auto response = m_impl->m_client.delete_(request);
+        status = Status(response.status(), response.status_message());
+    }
 
 }}
 
--- src/http_client.h
+++ src/http_client.h
@@ -6,19 +6,95 @@
 #include <utility>
 #include <vector>
 #include <stdint.h>
+#include <numeric>
+#include <type_traits>
 
 
 namespace ppconsul { namespace http {
 
-    class Options
+    namespace detail {
+
+//        inline std::string to_string(std::string v)
+//        {
+//            return std::move(v);
+//        }
+//
+//        inline std::string to_string(const char *v)
+//        {
+//            return {v};
+//        }
+
+        inline std::string to_string(char v)
+        {
+            return {1, v};
+        }
+
+        template<class T,
+            class Enabler = typename std::enable_if<std::is_arithmetic<T>::value>::type>
+        inline std::string to_string(T t)
+        {
+            return std::to_string(t);
+        }
+
+
+        class Parameter
+        {
+        public:
+            Parameter(std::string name, std::string value)
+            : m_name(std::move(name))
+            , m_value(std::move(value))
+            {}
+
+            template<class V,
+                class Disabler = typename std::enable_if<!std::is_constructible<std::string, V>::value>::type>
+            Parameter(std::string name, V value)
+            : m_name(std::move(name))
+            , m_value(to_string(value))
+            {}
+
+            std::string m_name, m_value;
+        };
+
+    }
+
+    class Parameters
     {
     public:
-        Options& operator() ();
+        using Parameter = detail::Parameter;
+
+        Parameters()
+        {}
+
+        /*template<class V>
+        Parameters(std::string name, V value)
+        {
+            m_values.emplace_back(std::move(name), std::move(value));
+        }*/
+
+        Parameters(std::initializer_list<Parameter> values)
+        : m_values(values)
+        {}
+
+        void append(std::initializer_list<Parameter> values)
+        {
+            m_values.insert(m_values.end(), values.begin(), values.end());
+        }
+
+        /*template<class V>
+        void append(std::string name, V value)
+        {
+            m_values.emplace_back(std::move(name), std::move(value));
+        }*/
+
+        bool empty() const { return m_values.empty(); }
+        void clear() { m_values.clear(); }
+
+        std::string query() const;
 
     private:
-        typedef std::pair<std::string, std::string> Value;
-        std::vector<Value> m_values;
+        std::vector<Parameter> m_values;
     };
+
 
     class Status
     {
@@ -33,7 +109,7 @@ namespace ppconsul { namespace http {
         , m_message(std::move(message))
         {}
 
-        // returns success()
+        // Returns success()
         explicit operator bool () const PPCONSUL_NOEXCEPT { return success(); }
 
         // Returns true if code() is 2xx (i.e. success) and false otherwise
@@ -47,10 +123,11 @@ namespace ppconsul { namespace http {
         std::string m_message;
     };
 
-    class StatusError: public std::exception
+
+    class BadStatus: public std::exception
     {
     public:
-        StatusError(Status status)
+        BadStatus(Status status)
         : m_status(std::move(status))
         {}
 
@@ -58,59 +135,79 @@ namespace ppconsul { namespace http {
 
         const Status& status() const PPCONSUL_NOEXCEPT { return m_status; }
 
-        // TODO: return string "<code> <message>"
-        virtual const char *what() const PPCONSUL_NOEXCEPT override { return m_status.message().c_str(); }
+        virtual const char *what() const PPCONSUL_NOEXCEPT override;
 
     private:
         Status m_status;
+        mutable std::string m_what;
     };
+
 
     class Client
     {
     public:
         explicit Client(const char *host = "http://localhost:8500");
+        ~Client();
 
-        // Throws StatusError on error
-        std::string get(const char *path, const Options& options = Options());
-        void put(const char *path, const std::string& body, const Options& options = Options());
-        void del(const char *path, const Options& options = Options());
+        std::string get(const char *path, const Parameters& parameters = Parameters());
+        std::string get(Status& status, const char *path, const Parameters& parameters = Parameters());
 
-        // Returns non-success status thru parameter
-        std::string get(Status& status, const char *path, const Options& options = Options());
-        void put(Status& status, const char *path, const std::string& body, const Options& options = Options());
-        void del(Status& status, const char *path, const Options& options = Options());
+        void put(const char *path, const std::string& body, const Parameters& parameters = Parameters());
+        void put(Status& status, const char *path, const std::string& body, const Parameters& parameters = Parameters());
+
+        void  del(const char *path, const Parameters& parameters = Parameters());
+        void  del(Status& status, const char *path, const Parameters& parameters = Parameters());
 
     private:
         class Impl;
+
+        std::string createUri(const char *path, const Parameters& parameters = Parameters()) const;
+
         std::unique_ptr<Impl> m_impl;
+        std::string m_host;
     };
 
 
     // Implementation
 
-    inline std::string Client::get(const char *path, const Options& options)
+    inline std::string Client::get(const char *path, const Parameters& parameters)
     {
         Status s;
-        auto r = get(s, path, options);
+        auto r = get(s, path, parameters);
         if (!s)
-            throw StatusError(std::move(s));
+            throw BadStatus(std::move(s));
         return r;
     }
 
-    inline void Client::put(const char *path, const std::string& body, const Options& options)
+    inline void Client::put(const char *path, const std::string& body, const Parameters& parameters)
     {
         Status s;
-        put(s, path, body, options);
+        put(s, path, body, parameters);
         if (!s)
-            throw StatusError(std::move(s));
+            throw BadStatus(std::move(s));
     }
 
-    inline void Client::del(const char *path, const Options& options)
+    inline void Client::del(const char *path, const Parameters& parameters)
     {
         Status s;
-        del(s, path, options);
+        del(s, path, parameters);
         if (!s)
-            throw StatusError(std::move(s));
+            throw BadStatus(std::move(s));
+    }
+
+    inline std::string Client::createUri(const char *path, const Parameters& parameters) const
+    {
+        auto query = parameters.query();
+
+        std::string r;
+        r.reserve(m_host.size() + strlen(path) + query.size() + 1); // 1 is for query prefix '?'
+
+        r += m_host;
+        r += path;
+        r += '?';
+        r += query;
+
+        return r;
     }
 
 }}
