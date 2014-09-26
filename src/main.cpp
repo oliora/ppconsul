39c3df9e95891422f0dae7905871ad7af8e43d26
Andrey Upadyshev <oliora@gmail.com>
2014-09-26 03:56:34 +0200

    Switch to use of HTTP client wrapper

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,19 @@
-#include <boost/network/protocol/http/client.hpp>
 #include <json11.hpp>
 #include <iostream>
-
-namespace ppconsul {
-
-    class Client
-    {
-    public:
-        explicit Client(const char *url = "http://localhost:8500")
-        : m_url(url)
-        {}
-
-    private:
-        std::string m_url;
-    };
-}
+#include "http_client.h"
 
 
 int main(int argc, char *argv[])
 {
-    using namespace boost::network;
-
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " [url]" << std::endl;
+    try
+    {
+        std::cout << ppconsul::http::Client().get(argv[1], {{"pretty", 1}}) << std::endl;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
         return 1;
     }
-
-    http::client client;
-    http::client::request request(argv[1]);
-    request << header("Connection", "close");
-    http::client::response response = client.get(request);
-    std::cout << body(response) << std::endl;
 
     return 0;
 }
