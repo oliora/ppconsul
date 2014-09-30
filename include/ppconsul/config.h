55c151327a5939c055bb1fd7205fa91a3e01591a
Andrey Upadyshev <oliora@gmail.com>
2014-09-30 11:43:35 +0200

    License and copyright info added

--- include/ppconsul/config.h
+++ include/ppconsul/config.h
@@ -1,3 +1,9 @@
+//  Copyright (c) 2014 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #if defined _MSC_VER && _MSC_VER < 1900  // MS Visual Studio before VS2014
--- src/http_client_impl_netlib.h
+++ src/http_client_impl_netlib.h
@@ -1,3 +1,9 @@
+//  Copyright (c) 2014 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include "ppconsul/http_status.h"
 #include <boost/network/protocol/http/client.hpp>
 
