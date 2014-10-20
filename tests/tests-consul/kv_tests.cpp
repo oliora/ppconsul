2bd19ae0f58516e02a6231a7999f4f9189c9b39c
Andrey Upadyshev <oliora@gmail.com>
2014-10-20 13:43:12 +0200

    Tests palced into separate folders. Empty file for agent tests added

--- tests/tests-consul/kv_tests.cpp
+++ tests/tests-consul/kv_tests.cpp
@@ -4,7 +4,6 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
 
 #include "ppconsul/kv.h"
---
+++ tests/unittests/main.cpp
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#define CATCH_CONFIG_MAIN
+#include <catch/catch.hpp>
