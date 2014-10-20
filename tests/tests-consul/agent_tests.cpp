28b1884af4215cc20f999f6f9a7e801f94132233
Andrey Upadyshev <oliora@gmail.com>
2014-10-20 16:28:24 +0200

    Tests for agent checks added

---
+++ tests/tests-consul/agent_tests.cpp
@@ -0,0 +1,318 @@
+//  Copyright (c) 2014 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <catch/catch.hpp>
+
+#include "ppconsul/agent.h"
+#include "test_consul.h"
+#include <thread>
+
+using ppconsul::agent::Agent;
+using ppconsul::agent::CheckStatus;
+
+namespace {
+    auto const Non_Existing_Script_Name = "63E7A7B1-FDAC-4D49-9F8F-1479C866815D";
+    auto const Unique_Id = "{16CA1AC9-72EE-451D-970E-E520B4EF874A}";
+    auto const Non_Existing_Check_Name = "DE2F4D40-2664-472D-B0B7-EA0A47D92136";
+}
+
+TEST_CASE("agent.check_deregistration", "[consul][agent][checks]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.registerCheck("check1", std::chrono::seconds(12));
+    REQUIRE(agent.checks().count("check1"));
+
+    agent.deregisterCheck("check1");
+    REQUIRE(!agent.checks().count("check1"));
+
+    CHECK_NOTHROW(agent.deregisterCheck(Non_Existing_Check_Name));
+}
+
+TEST_CASE("agent.check_registration", "[consul][agent][checks]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.deregisterCheck("check1");
+    agent.deregisterCheck(Unique_Id);
+
+    SECTION("ttl")
+    {
+        agent.registerCheck("check1", std::chrono::seconds(180));
+        
+        const auto checks = agent.checks();
+        REQUIRE(checks.count("check1"));
+        const auto & c = checks.at("check1");
+
+        CHECK(c.id == "check1");
+        CHECK(!c.node.empty());
+        CHECK(c.name == "check1");
+        CHECK(c.status == CheckStatus::Unknown);
+        CHECK(c.notes.empty());
+        CHECK(c.output.empty());
+        CHECK(c.serviceId.empty());
+        CHECK(c.serviceName.empty());
+    }
+
+    SECTION("ttl with notes")
+    {
+        agent.registerCheck("check1", std::chrono::seconds(180), "some notes");
+
+        const auto checks = agent.checks();
+        REQUIRE(checks.count("check1"));
+        const auto & c = checks.at("check1");
+
+        CHECK(c.id == "check1");
+        CHECK(!c.node.empty());
+        CHECK(c.name == "check1");
+        CHECK(c.status == CheckStatus::Unknown);
+        CHECK(c.notes == "some notes");
+        CHECK(c.output.empty());
+        CHECK(c.serviceId.empty());
+        CHECK(c.serviceName.empty());
+    }
+
+    SECTION("ttl with id")
+    {
+        agent.registerCheck("check1", std::chrono::seconds(180), "other notes", Unique_Id);
+
+        const auto checks = agent.checks();
+        REQUIRE(checks.count(Unique_Id));
+        const auto & c = checks.at(Unique_Id);
+
+        CHECK(c.id == Unique_Id);
+        CHECK(!c.node.empty());
+        CHECK(c.name == "check1");
+        CHECK(c.status == CheckStatus::Unknown);
+        CHECK(c.notes == "other notes");
+        CHECK(c.output.empty());
+        CHECK(c.serviceId.empty());
+        CHECK(c.serviceName.empty());
+    }
+
+    SECTION("script")
+    {
+        agent.registerCheck("check1", Non_Existing_Script_Name, std::chrono::seconds(100));
+        
+        const auto checks = agent.checks();
+        REQUIRE(checks.count("check1"));
+        const auto & c = checks.at("check1");
+
+        CHECK(c.id == "check1");
+        CHECK(!c.node.empty());
+        CHECK(c.name == "check1");
+        CHECK(c.notes.empty());
+        CHECK(c.status == CheckStatus::Warning);    // because of Non_Existing_Script_Name
+        CHECK(!c.output.empty());                    //
+        CHECK(c.serviceId.empty());
+        CHECK(c.serviceName.empty());
+    }
+
+    SECTION("script with notes")
+    {
+        agent.registerCheck("check1", Non_Existing_Script_Name, std::chrono::seconds(100), "the notes");
+
+        const auto checks = agent.checks();
+        REQUIRE(checks.count("check1"));
+        const auto & c = checks.at("check1");
+
+        CHECK(c.id == "check1");
+        CHECK(!c.node.empty());
+        CHECK(c.name == "check1");
+        CHECK(c.notes == "the notes");
+        CHECK(c.status == CheckStatus::Warning);    // because of Non_Existing_Script_Name
+        CHECK(!c.output.empty());                   //
+        CHECK(c.serviceId.empty());
+        CHECK(c.serviceName.empty());
+    }
+
+    SECTION("script with id")
+    {
+        agent.registerCheck("check1", Non_Existing_Script_Name, std::chrono::seconds(1), "the notes", Unique_Id);
+
+        const auto checks = agent.checks();
+        REQUIRE(checks.count(Unique_Id));
+        const auto & c = checks.at(Unique_Id);
+
+        CHECK(c.id == Unique_Id);
+        CHECK(!c.node.empty());
+        CHECK(c.name == "check1");
+        CHECK(c.notes == "the notes");
+        CHECK(c.status == CheckStatus::Warning);    // because of Non_Existing_Script_Name
+        CHECK(!c.output.empty());                   //
+        CHECK(c.serviceId.empty());
+        CHECK(c.serviceName.empty());
+    }
+}
+
+TEST_CASE("agent.check_registration_special_chars", "[consul][agent][checks][special chars]")
+{
+    const auto spec_name = "check{1}\r\n\t\x0";
+
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.deregisterCheck(spec_name);
+    REQUIRE(!agent.checks().count(spec_name));
+
+    SECTION("ttl")
+    {
+        agent.registerCheck(spec_name, std::chrono::seconds(100), "note\n1\n2\n3\nsummary: bla bla");
+
+        REQUIRE(agent.checks().count(spec_name));
+        auto c = agent.checks().at(spec_name);
+
+        CHECK(c.id == spec_name);
+        CHECK(c.name == spec_name);
+        CHECK(c.status == CheckStatus::Unknown);
+        CHECK(c.notes == "note\n1\n2\n3\nsummary: bla bla");
+    }
+    
+    SECTION("script")
+    {
+        agent.registerCheck(spec_name, Non_Existing_Script_Name, std::chrono::seconds(100));
+
+        REQUIRE(agent.checks().count(spec_name));
+        auto c = agent.checks().at(spec_name);
+
+        CHECK(c.id == spec_name);
+        CHECK(c.name == spec_name);
+        CHECK(c.status == CheckStatus::Warning);    // because of Non_Existing_Script_Name
+        CHECK(c.notes == "");
+    }
+}
+
+TEST_CASE("agent.check_update", "[consul][agent][checks][health]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.deregisterCheck("check1");
+    REQUIRE(!agent.checks().count("check1"));
+
+    agent.registerCheck("check1", std::chrono::minutes(5), "the check");
+    ppconsul::agent::Check c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Unknown);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "");
+    
+    agent.updateCheck("check1", CheckStatus::Failed, "it's failed :(");
+    c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Failed);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "it's failed :(");
+
+    agent.updateCheck("check1", CheckStatus::Passing, "status:\neverything passing!!!\n");
+    c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Passing);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "status:\neverything passing!!!\n");
+
+    agent.updateCheck("check1", CheckStatus::Passing);
+    c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Passing);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "");
+
+    agent.updateCheck("check1", CheckStatus::Warning);
+    c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Warning);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "");
+
+    CHECK_THROWS_AS(agent.updateCheck("check1", CheckStatus::Unknown), std::logic_error);
+}
+
+TEST_CASE("agent.check_update_incorrect", "[consul][agent][checks][health]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.deregisterCheck("check1");
+    REQUIRE(!agent.checks().count("check1"));
+
+    CHECK_THROWS_AS(agent.updateCheck(Non_Existing_Check_Name, CheckStatus::Passing), ppconsul::Error);
+
+    agent.registerCheck("check1", Non_Existing_Script_Name, std::chrono::minutes(5));
+
+    CHECK_THROWS_AS(agent.updateCheck("check1", CheckStatus::Passing), ppconsul::Error);
+}
+
+TEST_CASE("agent.check_expired", "[consul][agent][checks][health]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.deregisterCheck("check1");
+    REQUIRE(!agent.checks().count("check1"));
+
+    agent.registerCheck("check1", std::chrono::seconds(1));
+    REQUIRE(agent.checks().count("check1"));
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    auto c = agent.checks().at("check1");
+    CHECK(c.status == CheckStatus::Failed);
+    CHECK(!c.output.empty());
+}
+
+TEST_CASE("agent.check_update_special_chars", "[consul][agent][checks][health][special chars]")
+{
+    const auto spec_name = "check{1}\r\n\t\x0";
+
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+    agent.deregisterCheck(spec_name);
+    REQUIRE(!agent.checks().count(spec_name));
+
+    agent.registerCheck("check1", std::chrono::minutes(5), "the check");
+    ppconsul::agent::Check c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Unknown);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "");
+
+    agent.updateCheck("check1", CheckStatus::Passing, "status:\neverything passing!!!\n");
+    c = agent.checks().at("check1");
+    REQUIRE(c.status == CheckStatus::Passing);
+    REQUIRE(c.notes == "the check");
+    REQUIRE(c.output == "status:\neverything passing!!!\n");
+}
+
+/*
+TEST_CASE("agent.services", "[consul][agent][services]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+
+}
+
+TEST_CASE("agent.members", "[consul][agent][config][members]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+
+}
+
+TEST_CASE("agent.self", "[consul][agent][config][self]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+
+}
+
+TEST_CASE("agent.join_and_leave", "[consul][agent][config][join][leave]")
+{
+    auto consul = create_test_consul();
+    Agent agent(consul);
+
+
+}*/
