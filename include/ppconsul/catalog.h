a1b588714739332e6cebb8931f50fe5f441914e7
Andrey Upadyshev <oliora@gmail.com>
2014-10-21 17:30:02 +0200

    Catalog endpoint implemented (without registration/deregistration)

---
+++ include/ppconsul/catalog.h
@@ -0,0 +1,122 @@
+//  Copyright (c) 2014 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include "ppconsul/consul.h"
+#include "ppconsul/helpers.h"
+#include "ppconsul/types.h"
+
+
+namespace ppconsul { namespace catalog {
+
+    using ppconsul::Service;
+
+    struct Node
+    {
+        std::string name;
+        std::string address;
+    };
+
+    typedef std::pair<Node, std::map<std::string, Service>> NodeServices;
+
+    typedef std::pair<Service, Node> ServiceAndNode;
+
+    namespace params {
+        using ppconsul::params::consistency;
+        using ppconsul::params::block_for;
+
+        PPCONSUL_PARAM(tag, std::string)
+
+        namespace groups {
+            PPCONSUL_PARAMS_GROUP(get, (consistency, block_for))
+        }
+    }
+
+    namespace impl {
+        std::vector<std::string> parseDatacenters(const std::string& json);
+        std::vector<Node> parseNodes(const std::string& json);
+        NodeServices parseNode(const std::string& json);
+        std::map<std::string, Tags> parseServices(const std::string& json);
+        std::vector<ServiceAndNode> parseService(const std::string& json);
+    }
+
+    class Catalog
+    {
+    public:
+        // Allowed parameters:
+        // - consistency - default consistency for requests that support it
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        explicit Catalog(Consul& consul, const Params&... params)
+        : m_consul(consul)
+        , m_defaultConsistency(kwargs::get(params::consistency, Consistency::Default, params...))
+        {
+            KWARGS_CHECK_IN_LIST(Params, (params::consistency))
+        }
+
+        std::vector<std::string> datacenters() const
+        {
+            return impl::parseDatacenters(m_consul.get("/v1/catalog/datacenters"));
+        }
+
+        // Allowed parameters:
+        // - group::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        std::vector<Node> nodes(const Params&... params) const
+        {
+            KWARGS_CHECK_IN_GROUP(Params, params::groups::get)
+            return impl::parseNodes(m_consul.get("/v1/catalog/nodes", params::consistency = m_defaultConsistency, params...));
+        }
+
+        // Allowed parameters:
+        // - group::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        NodeServices node(const std::string& name, const Params&... params) const
+        {
+            KWARGS_CHECK_IN_GROUP(Params, params::groups::get)
+            return impl::parseNode(m_consul.get("/v1/catalog/node/" + helpers::encodeUrl(name),
+                params::consistency = m_defaultConsistency,
+                params...));
+        }
+
+        // Allowed parameters:
+        // - group::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        std::map<std::string, Tags> services(const Params&... params) const
+        {
+            KWARGS_CHECK_IN_GROUP(Params, params::groups::get)
+            return impl::parseServices(m_consul.get("/v1/catalog/services", params::consistency = m_defaultConsistency, params...));
+        }
+
+        // Allowed parameters:
+        // - group::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        std::vector<ServiceAndNode> service(const std::string& name, const Params&... params) const
+        {
+            KWARGS_CHECK_IN_GROUP(Params, params::groups::get)
+            return impl::parseService(m_consul.get("/v1/catalog/service/" + helpers::encodeUrl(name),
+                params::consistency = m_defaultConsistency,
+                params...));
+        }
+
+        // Allowed parameters:
+        // - group::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        std::vector<ServiceAndNode> service(const std::string& name, const std::string& tag, const Params&... params) const
+        {
+            KWARGS_CHECK_IN_GROUP(Params, params::groups::get)
+            return impl::parseService(m_consul.get("/v1/catalog/service/" + helpers::encodeUrl(name),
+                params::consistency = m_defaultConsistency,
+                params::tags = helpers::encodeUrl(tag),
+                params...));
+        }
+
+    private:
+        Consul& m_consul;
+
+        Consistency m_defaultConsistency;
+    };
+}}
---
+++ src/catalog.cpp
@@ -0,0 +1,65 @@
+//  Copyright (c) 2014 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "ppconsul/catalog.h"
+#include "s11n_types.h"
+
+
+namespace ppconsul { namespace catalog {
+    
+    using s11n::load;
+
+    void load(const s11n::Json& src, Node& dst)
+    {
+        load(src, dst.name, "Node");
+        load(src, dst.address, "Address");
+    }
+
+    void load(const s11n::Json& src, ServiceAndNode& dst)
+    {
+        load(src, dst.first.name, "ServiceName");
+        load(src, dst.first.port, "ServicePort");
+        load(src, dst.first.tags, "ServiceTags");
+        load(src, dst.first.id, "ServiceID");
+        load(src, dst.second);
+    }
+
+    void load(const s11n::Json& src, NodeServices& dst)
+    {
+        load(src, dst.first, "Node");
+        load(src, dst.second, "Services");
+    }
+
+namespace impl {
+
+    std::vector<std::string> parseDatacenters(const std::string& json)
+    {
+        return s11n::parseJson<std::vector<std::string>>(json);
+    }
+
+    std::vector<Node> parseNodes(const std::string& json)
+    {
+        return s11n::parseJson<std::vector<Node>>(json);
+    }
+
+    NodeServices parseNode(const std::string& json)
+    {
+        return s11n::parseJson<NodeServices>(json);
+    }
+
+    std::map<std::string, Tags> parseServices(const std::string& json)
+    {
+        return s11n::parseJson<std::map<std::string, Tags>>(json);
+    }
+
+    std::vector<ServiceAndNode> parseService(const std::string& json)
+    {
+        return s11n::parseJson<std::vector<ServiceAndNode>>(json);
+    }
+
+}
+}}
