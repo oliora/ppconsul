ef166fd769a118683a00cee6b85a4571038bf9a8
Andrey Upadyshev <oliora@gmail.com>
2014-11-13 01:49:20 +0100

    Health endpoint implemented (WIP, no tests yet)

--- include/ppconsul/health.h
+++ include/ppconsul/health.h
@@ -20,15 +20,23 @@ namespace ppconsul { namespace health {
         using ppconsul::params::consistency;
         using ppconsul::params::block_for;
         using ppconsul::params::dc;
+        using ppconsul::params::tag;
 
         PPCONSUL_PARAM(passing, bool);
 
         namespace groups {
-            PPCONSUL_PARAMS_GROUP(get, (consistency, dc, block_for))
+            PPCONSUL_PARAMS_GROUP(get, (consistency, dc, block_for));
         }
     }
 
-    std::tuple<Node, Service, std::vector<CheckInfo>> NodeServiceChecks;
+    typedef std::tuple<Node, Service, std::vector<CheckInfo>> NodeServiceChecks;
+
+    namespace impl {
+        std::vector<CheckInfo> parseCheckInfos(const std::string& json);
+        NodeServiceChecks parseService(const std::string& json);
+        std::string to_string(CheckStatus state);
+    }
+    
 
     class Health
     {
@@ -78,45 +86,65 @@ namespace ppconsul { namespace health {
         // Result contains both headers and data.
         // Allowed parameters:
         // - groups::get
+        // - params::tag
+        // - params::passing
         template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
-        Response<NodeServiceChecks> service(WithHeaders, const std::string& serviceName, const Params&... params) const
-        {
-            return service_impl(serviceName, params...);
-        }
+        Response<NodeServiceChecks> service(WithHeaders, const std::string& serviceName, const Params&... params) const;
 
         // Result contains data only.
         // Allowed parameters:
         // - groups::get
+        // - params::tag
+        // - params::passing
         template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
         NodeServiceChecks service(const std::string& serviceName, const Params&... params) const
         {
             return std::move(service(withHeaders, serviceName, params...).data());
         }
 
+        // Returns the checks in specified state
         // Result contains both headers and data.
         // Allowed parameters:
         // - groups::get
         template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
-        Response<NodeServiceChecks> service(WithHeaders, const std::string& serviceName, const std::string& tag, const Params&... params) const
+        Response<std::vector<CheckInfo>> state(WithHeaders, CheckStatus state, const Params&... params) const
         {
-            return service_impl(serviceName, params...);
+            return state_impl(impl::to_string(state), params...);
         }
 
+        // Returns the checks in specified state
         // Result contains data only.
         // Allowed parameters:
         // - groups::get
         template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
-        NodeServiceChecks service(const std::string& serviceName, const std::string& tag, const Params&... params) const
+        std::vector<CheckInfo> state(CheckStatus state, const Params&... params) const
         {
-            return std::move(service(withHeaders, serviceName, tag, params...).data());
+            return std::move(state(withHeaders, state, params...).data());
+        }
+
+        // Returns all the checks
+        // Result contains both headers and data.
+        // Allowed parameters:
+        // - groups::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        Response<std::vector<CheckInfo>> state(WithHeaders, const Params&... params) const
+        {
+            return state_impl("any", params...);
+        }
+
+        // Returns all the checks
+        // Result contains data only.
+        // Allowed parameters:
+        // - groups::get
+        template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
+        std::vector<CheckInfo> state(const Params&... params) const
+        {
+            return std::move(state(withHeaders, params...).data());
         }
 
     private:
         template<class... Params, class = kwargs::enable_if_kwargs_t<Params...>>
-        Response<NodeServiceChecks> service_impl(const std::string& serviceName, const Params&... params) const
-        {
-
-        }
+        Response<std::vector<CheckInfo>> state_impl(const std::string& state, const Params&... params) const;
 
         Consul& m_consul;
 
@@ -127,19 +155,61 @@ namespace ppconsul { namespace health {
 
     // Implementation
 
+    inline std::string impl::to_string(CheckStatus state)
+    {
+        switch (state)
+        {
+            case CheckStatus::Unknown:
+                return "unknown";
+            case CheckStatus::Passing:
+                return "passing";
+            case CheckStatus::Warning:
+                return "warning";
+            case CheckStatus::Critical:
+                return "critical";
+            default:
+                throw std::logic_error("Wrong CheckStatus value");
+        }
+    }
+
     template<class... Params, class>
     Response<std::vector<CheckInfo>> Health::node(WithHeaders, const std::string& name, const Params&... params) const
-    {}
+    {
+        KWARGS_CHECK_IN_LIST(Params, (params::groups::get));
+        auto r = m_consul.get(withHeaders, "/v1/health/node/" + helpers::encodeUrl(name),
+                              params::consistency = m_defaultConsistency, params::dc = m_defaultDc,
+                              params...);
+        return makeResponse(r.headers(), impl::parseCheckInfos(r.data()));
+    }
 
     template<class... Params, class>
     Response<std::vector<CheckInfo>> Health::checks(WithHeaders, const std::string& serviceName, const Params&... params) const
-    {}
+    {
+        KWARGS_CHECK_IN_LIST(Params, (params::groups::get));
+        auto r = m_consul.get(withHeaders, "/v1/health/checks/" + helpers::encodeUrl(serviceName),
+                              params::consistency = m_defaultConsistency, params::dc = m_defaultDc,
+                              params...);
+        return makeResponse(r.headers(), impl::parseCheckInfos(r.data()));
+    }
 
     template<class... Params, class>
     Response<NodeServiceChecks> Health::service(WithHeaders, const std::string& serviceName, const Params&... params) const
-    {}
+    {
+        KWARGS_CHECK_IN_LIST(Params, (params::groups::get, params::tag, params::passing));
+        auto r = m_consul.get(withHeaders, "/v1/health/service/" + helpers::encodeUrl(serviceName),
+                              params::consistency = m_defaultConsistency, params::dc = m_defaultDc,
+                              params...);
+        return makeResponse(r.headers(), impl::parseService(r.data()));
+    }
 
     template<class... Params, class>
-    Response<NodeServiceChecks> Health::service(WithHeaders, const std::string& serviceName, const std::string& tag, const Params&... params) const
-    {}
+    Response<std::vector<CheckInfo>> Health::state_impl(const std::string& state, const Params&... params) const
+    {
+        KWARGS_CHECK_IN_LIST(Params, (params::groups::get));
+        auto r = m_consul.get(withHeaders, "/v1/health/state/" + state,
+                              params::consistency = m_defaultConsistency, params::dc = m_defaultDc,
+                              params...);
+        return makeResponse(r.headers(), impl::parseCheckInfos(r.data()));
+    }
+
 }}
