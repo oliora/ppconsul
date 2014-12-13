b48df3053c1a5d675e6a350e53f74a96a7290046
Andrey Upadyshev <oliora@gmail.com>
2014-12-13 22:45:43 +0100

    Test consul start/stop script for Windows

---
+++ tests/test-consul.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set DATA_DIR=test-consul-data
+
+if "%1"=="start" (
+    consul info > nul
+    if NOT ERRORLEVEL 1 (echo Consul is already running && exit /b 1)
+    start consul agent -server -bootstrap-expect=1 "-data-dir=%DATA_DIR%"
+    timeout 3
+    consul info > nul
+    if ERRORLEVEL 1 (echo Can not start Consul && exit /b 1)
+    echo Consul started successfully
+    exit /b 0
+)
+
+if "%1"=="stop" (
+    consul info > nul
+    if ERRORLEVEL 1 (echo Consul is not running && exit /b 0)
+    consul leave
+    if ERRORLEVEL 1 exit /b 1
+    exit /b 0
+)
+
+echo Usage: test-consul start^|stop
+exit /b 2
