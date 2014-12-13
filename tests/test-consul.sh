ff32f06f8d02d2ce2f59fadbb090a2211791f1f8
Andrey Upadyshev <oliora@gmail.com>
2014-12-13 14:06:31 +0100

    Tests: wrong location of Consul data

--- tests/test-consul.sh
+++ tests/test-consul.sh
@@ -4,7 +4,7 @@
 case "$1" in
 start)
     mkdir -p /tmp/test-consul/data || exit 1
-    consul agent -bootstrap-expect=1 -server -dc=ppconsul_test -data-dir=~/tmp/test-consul/data -pid-file=/tmp/test-consul/pid  >/dev/null &
+    consul agent -bootstrap-expect=1 -server -dc=ppconsul_test -data-dir=/tmp/test-consul/data -pid-file=/tmp/test-consul/pid  >/dev/null &
     sleep 3s
     if [ -e "/tmp/test-consul/pid" ]; then
         echo "test consul started, PID=$(</tmp/test-consul/pid)"
