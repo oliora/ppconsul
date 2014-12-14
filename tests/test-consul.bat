4ba14109a8f5412d8d41c860ae22084cd1aee265
Andrey Upadyshev <oliora@gmail.com>
2014-12-14 21:52:56 +0100

    Test: Use ping instead of timeout to do sleep on Windows

--- tests/test-consul.bat
+++ tests/test-consul.bat
@@ -7,7 +7,8 @@ if "%1"=="start" (
     if NOT ERRORLEVEL 1 (echo Consul is already running && exit /b 1)
     if EXIST "%DATA_DIR%" (rd /S /Q "%DATA_DIR%" || echo Can not delete data dir && exit /b 1)
     start consul agent -server -bootstrap-expect=1 -dc=ppconsul_test "-data-dir=%DATA_DIR%"
-    timeout 3
+    rem Use ping to sleep for 3 seconds
+    ping 127.0.0.1 -n 3 > nul
     consul info > nul
     if ERRORLEVEL 1 (echo Can not start Consul && exit /b 1)
     echo Consul started successfully
