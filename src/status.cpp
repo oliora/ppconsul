3b7419a86b0044b7108c3b0fbf5f4d966dec8d95
Chris Love <cl186051@teradata.com>
2018-03-17 21:36:07 -0700

    Initial support for the status endpoint - leader() returns the Consul leader (or empty string) - peers() returns the raft peers

---
+++ src/status.cpp
@@ -0,0 +1,26 @@
+//  Copyright (c) 2014-2017 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "ppconsul/status.h"
+#include "s11n_types.h"
+
+namespace ppconsul { namespace status {
+
+namespace impl {
+
+	std::string parseLeader(const std::string &json)
+	{
+		return s11n::parseJson<std::string>(json);
+	}
+
+	std::vector<std::string> parsePeers(const std::string& json)
+	{
+		return s11n::parseJson<std::vector<std::string>>(json);
+	}
+
+}
+}}
