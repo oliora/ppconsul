6e2637a993f07714bda7fb70118793b9f54e8a2b
Chris Love <cl186051@teradata.com>
2018-03-19 09:34:12 -0700

    Formatting cleanups, use boost::optional for status::leader()

--- src/status.cpp
+++ src/status.cpp
@@ -12,15 +12,15 @@ namespace ppconsul { namespace status {
 
 namespace impl {
 
-	std::string parseLeader(const std::string &json)
-	{
-		return s11n::parseJson<std::string>(json);
-	}
+    std::string parseLeader(const std::string& json)
+    {
+        return s11n::parseJson<std::string>(json);
+    }
 
-	std::vector<std::string> parsePeers(const std::string& json)
-	{
-		return s11n::parseJson<std::vector<std::string>>(json);
-	}
+    std::vector<std::string> parsePeers(const std::string& json)
+    {
+        return s11n::parseJson<std::vector<std::string>>(json);
+    }
 
 }
 }}
