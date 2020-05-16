449ed724c3273aadc0ed18743d8653afd53d8505
Andrey Upadyshev <oliora@gmail.com>
2020-05-16 14:02:13 +0200

    Update copyright year to 2020

--- src/client_pool.cpp
+++ src/client_pool.cpp
@@ -1,3 +1,9 @@
+//  Copyright (c)  2020 Andrey Upadyshev <oliora@gmail.com>
+//
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <ppconsul/client_pool.h>
 
 
